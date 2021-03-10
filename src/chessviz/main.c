d48a0b3b62f246b289c0d30d84569a69a69feae1
Vyacheslav Kotov <vekotov@users.noreply.github.com>
2021-03-10 16:15:51 +0700

    Add moves file opening

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -7,8 +7,20 @@
 
 int chessboard[8][8];
 
-int main()
+int main(int argc, char **argv)
 {
+    if(argc == 0) {
+    	fprintf("Error: Incorrect use. Use: chessviz <file>");
+    	return 1;
+    }
+    
+    FILE* moves = fopen(argv);
+    
+    if(moves == NULL) {
+    	fprintf("Error: Cant open file");
+    	return 2;
+    }
+    
     fillChessboard(chessboard);
     mkdir("result", 0750);
     printChessboard(chessboard, fopen("result/1.html", "w"));
